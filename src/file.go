ee59c76aa7ce7b1ea5ad4a802b50e3d822083244
Tiago Ferreira <ferreiratiago@outlook.com>
2023-02-04 11:22:47 +0000

    tryout

--- src/file.go
+++ src/file.go
@@ -4,4 +4,18 @@ import "fmt"
 
 func main() {
 	fmt.Println("Hello, world! x5")
+	for i := 0; i < 10; i++ {
+		if i%2 == 0 {
+			if i%3 == 0 {
+				fmt.Println(i, "is even and divisible by 3")
+			} else {
+				if i%4 == 0 {
+					fmt.Println(i, "is even and divisible by 4")
+				}
+			}
+			fmt.Println(i, "is even")
+		} else {
+			fmt.Println(i, "is odd")
+		}
+	}
 }
